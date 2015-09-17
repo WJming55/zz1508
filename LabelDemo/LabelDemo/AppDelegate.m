22dc6d28fc0e17ef2460583345b574e948977982
WJming55 <2438497343@qq.com>
2015-09-17 10:43:56 +0800

    weweuyu
    yuyu

--- LabelDemo/LabelDemo/AppDelegate.m
+++ LabelDemo/LabelDemo/AppDelegate.m
@@ -54,7 +54,7 @@
     [self.window makeKeyAndVisible];
     
     return YES;
-}
+}//////////////////////////////111111111111111
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
